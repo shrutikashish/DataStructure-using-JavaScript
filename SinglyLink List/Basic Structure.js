e2180fb48f74e05c81898c6355906e8ed8b00a54
DESKTOP-L14UOS9\Shruti <shruti.kashish8@gmail.com>
2022-02-06 20:42:22 +0530

    Basic structure of sigly link list

---
+++ SinglyLink List/Basic Structure.js
@@ -0,0 +1,18 @@
+class Node{
+    constructor(val){
+        this.data=val;
+        this.next=null;
+
+    }
+}
+
+class Singlelinklist{
+    constructor(){
+        
+        this.head=null
+        this.tail=null
+        this.length=0
+    }
+
+}
+const b=new Singlelinklist() 
